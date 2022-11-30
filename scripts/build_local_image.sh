83d20f3c7e84ec5566da00895a74d58536c734e5
peak3d <peak3d@videvago.com>
2022-11-30 19:54:03 +0100

    [DOCKER] 1.17 -> 1.18 + submodule

--- scripts/build_local_image.sh
+++ scripts/build_local_image.sh
@@ -10,5 +10,8 @@ CAMINO_NODE_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd .. && pwd )
 # Load the constants
 source "$CAMINO_NODE_PATH"/scripts/constants.sh
 
+# Prepare dependencies
+git submodule update --init
+
 echo "Building Docker Image with tag $camino_node_dockerhub_repo:$current_branch"
 docker build -t "$camino_node_dockerhub_repo:$current_branch" "$CAMINO_NODE_PATH" -f "$CAMINO_NODE_PATH/Dockerfile"
