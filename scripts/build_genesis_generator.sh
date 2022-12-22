84649d5ceb08759c9fe3d5a4b2adca97b5239ac5
peak3d <peak3d@videvago.com>
2022-12-22 10:40:17 +0100

    Genesis generator as camino-node tool (#36) / caminoethvm bump

---
+++ scripts/build_genesis_generator.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+echo "Building Genesis Generator..."
+
+CAMINO_NODE_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd .. && pwd )
+source "$CAMINO_NODE_PATH"/scripts/constants.sh
+
+target="$build_dir/genesis-generator"
+go build -o "$target" "$CAMINO_NODE_PATH/tools/genesis/"*.go
