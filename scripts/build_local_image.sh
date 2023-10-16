31b8a71da8b584530f091a9b4b87b6248828cff6
Kirsten Schulte <kirsten@e-schulte.net>
2023-10-16 18:01:46 +0200

    [Improvment] Creating the local docker image on Darwin/arm64 processor environments will work directly

--- scripts/build_local_image.sh
+++ scripts/build_local_image.sh
@@ -17,4 +17,4 @@ fi
 source "$CAMINO_NODE_PATH"/scripts/constants.sh
 
 echo "Building Docker Image with tag $camino_node_dockerhub_repo:$current_branch"
-docker build -t "$camino_node_dockerhub_repo:$current_branch" "$CAMINO_NODE_PATH" -f "$CAMINO_NODE_PATH/Dockerfile"
+docker build --platform linux/amd64 -t "$camino_node_dockerhub_repo:$current_branch" "$CAMINO_NODE_PATH" -f "$CAMINO_NODE_PATH/Dockerfile"
